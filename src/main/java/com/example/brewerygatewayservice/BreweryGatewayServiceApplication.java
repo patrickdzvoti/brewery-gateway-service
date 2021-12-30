a0dd238ffce37d977e9b0206f5cc2f380b96cc0b
patrickdzvoti <pdzvoti@gmail.com>
2021-12-30 01:03:55 +0000

    Initial commit

---
+++ src/main/java/com/example/brewerygatewayservice/BreweryGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.brewerygatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreweryGatewayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BreweryGatewayServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/brewerygatewayservice/BreweryGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.brewerygatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreweryGatewayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
