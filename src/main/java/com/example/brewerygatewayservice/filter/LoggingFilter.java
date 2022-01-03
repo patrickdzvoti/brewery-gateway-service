494527b67492722caa3247adec27f3d9747fbaf4
patrickdzvoti <pdzvoti@gmail.com>
2022-01-03 01:44:20 +0000

    Routes configuration

---
+++ src/main/java/com/example/brewerygatewayservice/filter/LoggingFilter.java
@@ -0,0 +1,37 @@
+package com.example.brewerygatewayservice.filter;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.cloud.gateway.route.Route;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.net.URI;
+import java.util.Collections;
+import java.util.Set;
+
+import static org.springframework.cloud.gateway.support.ServerWebExchangeUtils.*;
+
+/**
+ * @author Patrick Dzvoti
+ * Initial 30/12/2021 - 09:46
+ */
+@Component
+public class LoggingFilter implements GlobalFilter {
+
+    Log log = LogFactory.getLog(this.getClass());
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        Set<URI> uris = exchange.getAttributeOrDefault(GATEWAY_ORIGINAL_REQUEST_URL_ATTR, Collections.emptySet());
+        String originalUri = (uris.isEmpty()) ? "Unknown" : uris.iterator().next().toString();
+        Route route = exchange.getAttribute(GATEWAY_ROUTE_ATTR);
+        URI routeUri = exchange.getAttribute(GATEWAY_REQUEST_URL_ATTR);
+        log.info("Incoming request " + originalUri + " is routed to id: " + route.getId()
+                + ", uri:" + routeUri);
+        return chain.filter(exchange);
+    }
+}
